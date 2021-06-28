149b7de5efdb057bf23f0d6d0b4234754b752ebe
Dmitry <dim.smir@inbox.ru>
2021-06-28 10:38:54 +0600

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ title }}</h1>
+    <div class="operands">
+      <input v-model="operand1" type="text" placeholder="Число 1" />
+      <input v-model="operand2" type="text" placeholder="Число 2" />
+      <h3>Результат: {{ result }}</h3>
+    </div>
+    <div class="buttons">
+      <button @click="result = +operand1 + +operand2">+</button>
+      <button @click="result = +operand1 - +operand2">-</button>
+      <button @click="result = +operand1 * +operand2">*</button>
+      <button @click="result = +operand1 / +operand2">/</button>
+      <button @click="result = Math.pow(operand1, operand2)">
+        Число 1 в степени числа 2
+      </button>
+      <button @click="result = Math.floor(+operand1 / +operand2)">
+        % (деление без остатка)
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "Калькулятор",
+      operand1: "",
+      operand2: "",
+      result: 0,
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+h1,
+h3 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.operands {
+  width: 350px;
+  margin: 0 auto;
+}
+.buttons {
+  width: 500px;
+  margin: 0 auto;
+}
+
+button:not(:first-child) {
+  margin-left: 5px;
 }
 </style>
